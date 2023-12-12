a796cc5b00c4b19808b8b8e7cc13963f8cc1de62
Anderson <loonander@gmail.com>
2023-12-12 17:35:18 -0300

    condição de erro

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 let display = document.getElementById('display')
 
-function addNumber(number){
-    display.value += number
+function addNumber(number) {
+    const operators = ['+', '-', 'x', '/'];
+    const lastChar = display.value.charAt(display.value.length - 1);
+
+    if (operators.includes(lastChar) && operators.includes(number)) {
+        // Se o último caractere e o número a ser adicionado são operadores, substitua o último operador pelo novo
+        display.value = display.value.slice(0, -1) + number;
+    } else {
+        // Adicione o número normalmente
+        display.value += number;
+    }
 }
 
 
 function calculate(){
-    try {
-        display.value = eval(display.value)
-    } catch (error) {
-        display.value = 'Error'
+    if(display.value){
+        try {
+            display.value = eval(display.value)
+        } catch (error) {
+            display.value = ''
+            display.placeholder='Error'
+        }
     }
+   
    
 }
 
