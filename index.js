6d0e720207e4cabad9292ffb9e7b62adb905bc04
Anderson <loonander@gmail.com>
2023-12-12 16:59:59 -0300

    javascript

---
+++ index.js
@@ -0,0 +1,25 @@
+let display = document.getElementById('')
+
+function addNumber(number){
+    display.value += number
+
+}
+
+
+function calculate(){
+    try {
+        display.value = eval(display.value)
+    } catch (error) {
+        display.value = 'Error'
+    }
+   
+}
+
+
+function clean(){
+    display.value = ''
+}
+
+function backspace(){
+    display.value = result.value.slice(0,-1)
+}
