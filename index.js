13ee723c8fb38ff824dd3018ae6c0df86ec7ae96
Anderson <loonander@gmail.com>
2023-12-14 15:17:23 -0300

    concertado

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let display = document.getElementById('display')
 
 function addNumber(number) {
-    const operators = ['+', '-', 'x', '/'];
+    const operators = ['+', '-', '*', '/'];
     const lastChar = display.value.charAt(display.value.length - 1);
 
     if (operators.includes(lastChar) && operators.includes(number)) {
-        // Se o último caractere e o número a ser adicionado são operadores, substitua o último operador pelo novo
+      
         display.value = display.value.slice(0, -1) + number;
     } else {
-        // Adicione o número normalmente
+       
         display.value += number;
     }
 }
